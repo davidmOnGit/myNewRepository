f9c628746fecd1fac16d572b14eb6c054f13e72d
David <david.mager2@mailbox.tu-dresden.de>
2013-03-14 23:27:10 +0100

    first

---
+++ T2Shop/target/T2Shop-0.0.1-SNAPSHOT/WEB-INF/classes/de/david/t2rwin/spring/AppConfig.java
@@ -0,0 +1,10 @@
+package de.david.t2rwin.services;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:/de/david/t2rwin/spring/applicationContext.xml")
+public class AppConfig {
+
+}
